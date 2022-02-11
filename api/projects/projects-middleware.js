c489a5a7bc8d4a48482e4cca824c19b1fca36009
Ryan Howard <ryanghoward@protonmail.com>
2022-02-10 19:22:33 -0800

    Projects router and middleware done

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,35 @@
 // add middlewares here related to projects
+const Actions = require("./actions-model");
+
+async function validateId(req, res, next) {
+  try {
+    const { id } = req.params;
+    const actions = await Actions.get(id);
+    if (!actions) {
+      next({ status: 404, message: "No such user found :(" });
+    } else {
+      req.actions = actions;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "No such action found :(",
+    });
+  }
+}
+
+async function validateBody(req, res, next) {
+  const { completed, notes, description, project_id } = req.body;
+  if (!notes || !project_id || !description) {
+    res.status(400).json({
+      message: "Notes, Project ID, or Description are missing :(",
+    });
+  } else {
+    req.completed = completed;
+    req.notes = notes;
+    req.description = description;
+    req.project_id = project_id;
+  }
+}
+
+module.exports = { validateBody, validateId };
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,67 @@
 // Write your "projects" router here!
+const Project = require("./projects-model");
+const express = require("express");
+const router = express.Router();
+const { validateId, validateBody } = require("./projects-middleware");
+
+// Get
+router.get("/", (req, res, next) => {
+  Project.get()
+    .then((projects) => {
+      res.status(201).json(projects);
+    })
+    .catch(next);
+});
+
+// Get ID
+router.get("/:id", validateId, (req, res) => {
+  res.json(req.projects);
+});
+
+// Post
+router.post("/", validateBody, (req, res, next) => {
+  Project.insert({
+    name: req.name,
+    description: req.description,
+    completed: req.completed,
+  })
+    .then((projects) => {
+      res.status(201).json(projects);
+    })
+    .catch(next);
+});
+
+// Put ID
+router.put("/:id", validateId, validateBody, async (req, res, next) => {
+  let changes = { ...req.body, completed: req.completed };
+  const updateProject = await Project.update(req.params.id, changes);
+  try {
+    res.status(400).json(updateProject);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Delete ID
+router.delete("./id", validateId, async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const results = await Project.remove(id);
+    res.status(201).json(results);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Get ID Actions
+router.get("/:id/actions", validateId, (req, res, next) => {
+  Project.getProjectActions(req.params.id)
+    .then((actions) => {
+      {
+        res.json(actions);
+      }
+    })
+    .catch(next);
+});
+
+module.exports = router;
